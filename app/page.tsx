a760bb1a23e2531a454c5b22cdb5450b054e74a6
roelmaoo <roeldocto8@gmail.com>
2026-06-15 22:35:13 +0800

    fixed light and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -18,41 +18,41 @@ function App() {
         <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] md: gap-3">
           {/* left */}
           <div className="flex flex-col gap-3">
-            <div className="bg-[#161616] p-4 rounded-md border border-[#353535] flex flex-col gap-3">
+            <div className="bg-[var(--card)] p-4 rounded-md border border-[var(--border)] flex flex-col gap-3">
               <div className="bg-gray-500 border-2 border-dashed rounded-xl w-16 h-16" />
               <p className="font-semibold">Roel Docto Jr.</p>
-              <p className="text-sm text-white/50 font-light leading-4">
+              <p className="text-sm text-[var(--muted)] font-light leading-4">
                 Frontend Developer | UI/UX Designer | keyboard Enthusiast. Dog
                 lover
               </p>
             </div>
 
             {/* tech stack */}
-            <div className="flex grow flex-col gap-3 rounded-md border border-[#353535] bg-[#161616] p-4">
-              <p className="text-xs font-semibold tracking-wider text-neutral-400">
+            <div className="flex grow flex-col gap-3 rounded-md border border-[var(--border)] bg-[var(--card)] p-4">
+              <p className="text-xs font-semibold tracking-wider text-[var(--muted)]">
                 TECH STACK
               </p>
 
               <div className="flex flex-wrap gap-2">
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727] text-[#neutral-200]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)] text-[var(--muted)]">
                   <SiReact size={20} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiNextdotjs size={20} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiTailwindcss size={20} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiTypescript size={18} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiNodedotjs size={20} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiPostgresql size={20} />
                 </div>
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[#353535] bg-[#272727]">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-[var(--border)] bg-[var(--card)]">
                   <SiPrisma size={20} />
                 </div>
               </div>
@@ -60,9 +60,9 @@ function App() {
           </div>
 
           {/* mid card */}
-          <div className="flex flex-col justify-between bg-[#161616] rounded-md p-4 border border-[#353535] gap-5">
+          <div className="flex flex-col justify-between bg-[var(--card)] rounded-md p-4 border border-[var(--border)] gap-5">
             <div className="flex flex-col grow gap-2">
-              <div className="flex items-center gap-2 text-sm text-white/50 uppercase tracking-wider">
+              <div className="flex items-center gap-2 text-sm text-[var(--muted)] uppercase tracking-wider">
                 <p>Latest Project</p>
               </div>
               <div className="flex bg-white/10 rounded-md justify-center items-center">
@@ -77,17 +77,17 @@ function App() {
               </div>
               <div className="flex flex-1 flex-col">
                 <p className="font-semibold">Iloilo Jeepney Routes</p>
-                <p className="text-sm text-white/50">
+                <p className="text-sm text-[var(--muted)]">
                   A web application that provides real-time information on
                   jeepney routes
                 </p>
               </div>
             </div>
 
-            <div className="flex items-center justify-center mt-10">
+            <div className="flex bg-[var(--button)] rounded-md justify-center items-center">
               <a
-                className="flex grow justify-center bg-[#272727] border border-[#353535] rounded py-2
-              hover:bg-[#2c2c2c] cursor-pointer"
+                className="flex grow justify-center bg-[var(--button)] border border-[var(--border)] rounded py-2
+              hover:bg-[var(--button-hover)] cursor-pointer"
                 href="https://iloilojeepneyroutes.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -99,7 +99,7 @@ function App() {
 
           {/* right */}
           <div className="flex flex-col gap-3">
-            <div className="bg-[#161616] p-4 rounded-md border border-[#353535]">
+            <div className="bg-[var(--card)] p-4 rounded-md border border-[var(--border)]">
               <p className="text-xl font-bold">Projects</p>
             </div>
 
@@ -114,11 +114,11 @@ function App() {
                 className="flex flex-col gap-3"
               >
                 <div
-                  className="bg-[#161616] p-4 rounded-md border border-[#353535]
-            hover:bg-[#1d1d1d] hover:border-[#424242] cursor-pointer transition-all"
+                  className="bg-[var(--card)] p-4 rounded-md border border-[var(--border)]
+            hover:bg-[var(--card-hover)] hover:border-[var(--border)] cursor-pointer transition-all"
                 >
                   <div>
-                    <p className="pb-2 text-sm text-white/50 uppercase tracking-wider">
+                    <p className="pb-2 text-sm text-[var(--muted)] uppercase tracking-wider">
                       STRAY DOGS LANDING PAGE
                     </p>
                     <Image
@@ -129,7 +129,7 @@ function App() {
                       width={800}
                       height={0}
                     />
-                    <p className="text-sm text-white/50 pt-2 uppercase">
+                    <p className="text-sm text-[var(--muted)] pt-2 uppercase">
                       A landing page for stray pets where users can book strays
                       for a walk, or adoption.
                     </p>
@@ -144,8 +144,8 @@ function App() {
                 className="flex flex-col gap-3"
               >
                 <div
-                  className="bg-[#161616] p-4 rounded-md border border-[#353535]
-            hover:bg-[#1d1d1d] hover:border-[#424242] cursor-pointer transition-all"
+                  className="bg-[var(--card)] p-4 rounded-md border border-[var(--border)]
+            hover:bg-[var(--card-hover)] hover:border-[var(--border)] cursor-pointer transition-all"
                 >
                   <div>
                     <p className="pb-2">Project 1</p>
@@ -157,7 +157,7 @@ function App() {
                       width={800}
                       height={0}
                     />
-                    <p className="text-sm text-white/50 pt-2">
+                    <p className="text-sm text-[var(--muted)] pt-2">
                       A landing page for stray pets where users can book strays
                       for a walk, or adoption.
                     </p>
@@ -172,8 +172,8 @@ function App() {
                 className="flex flex-col gap-3"
               >
                 <div
-                  className="bg-[#161616] p-4 rounded-md border border-[#353535]
-            hover:bg-[#1d1d1d] hover:border-[#424242] cursor-pointer transition-all"
+                  className="bg-[var(--card)] p-4 rounded-md border border-[var(--border)]
+            hover:bg-[var(--card-hover)] hover:border-[var(--border)] cursor-pointer transition-all"
                 >
                   <div>
                     <p className="pb-2">Project 1</p>
@@ -185,7 +185,7 @@ function App() {
                       width={800}
                       height={0}
                     />
-                    <p className="text-sm text-white/50 pt-2">
+                    <p className="text-sm text-[var(--muted)] pt-2">
                       A landing page for stray pets where users can book strays
                       for a walk, or adoption.
                     </p>
